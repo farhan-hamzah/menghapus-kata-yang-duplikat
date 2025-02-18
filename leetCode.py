07706a8d0dfc48f0871b25f907385a7492528c13
Farhan Hamzah <177802321+farhan-hamzah@users.noreply.github.com>
2025-02-18 08:45:25 +0700

    Add files via upload

---
+++ leetCode.py
@@ -0,0 +1,7 @@
+# kata = str(input())
+# words = kata.split()
+# result = []
+# for i in range(len(words)):
+#     if i == 0 or words[i] != words[i - 1]:
+#         result.append(words[i])
+# print(result)
